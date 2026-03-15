c317299e68a73a01e79fc11f072473fb5e473383
tawhidzihad <tawhidzihadb1@gmail.com>
2026-03-15 06:50:20 +0600

    All countries loaded through api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Suspense } from "react";
 import "./App.css";
 import Countries from "./components/Countries/Countries";
+
+const countriesPromise = fetch(
+	"https://openapi.programming-hero.com/api/all",
+).then((res) => res.json());
 
 function App() {
 	return (
 		<>
-			<h1>In The Countries</h1>
-			<Countries></Countries>
+			<Suspense fallback={<p>Countries Loading....</p>}>
+				<Countries countriesPromise={countriesPromise}></Countries>
+			</Suspense>
 		</>
 	);
 }
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import { use } from "react";
 
-const Countries = () => {
-   return (
-      <div>
-         
-      </div>
-   );
+const Countries = ({ countriesPromise }) => {
+	const countriesData = use(countriesPromise);
+	const countries = countriesData.countries;
+
+	console.log(countries);
+	return (
+		<div>
+			<h1>In The Countries {countries.length}</h1>
+		</div>
+	);
 };
 
 export default Countries;
