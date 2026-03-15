05d0c4cef693295919ce44440328a3f1a4e249ec
tawhidzihad <tawhidzihadb1@gmail.com>
2026-03-15 07:44:50 +0600

    Country currencies added with currecy name and symbol

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,13 +1,16 @@
 import { use } from "react";
+import Country from "../Country/Country";
 
 const Countries = ({ countriesPromise }) => {
 	const countriesData = use(countriesPromise);
 	const countries = countriesData.countries;
 
-	console.log(countries);
 	return (
 		<div>
 			<h1>In The Countries {countries.length}</h1>
+			{countries.map((country) => (
+				<Country key={country.cca3.cca3} country={country}></Country>
+			))}
 		</div>
 	);
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+const Country = ({ country }) => {
+	const currenciesCode = Object.keys(country.currencies?.currencies)[0];
+	const currencyName = country.currencies?.currencies[currenciesCode]?.name;
+	const currencySymbol =
+		country.currencies?.currencies[currenciesCode]?.symbol;
 
-const Country = () => {
-   return (
-      <div>
-         
-      </div>
-   );
+	// console.log(currenciesCode[0]);
+
+	return (
+		<div className="containerStye">
+			<img src={country.flags?.flags?.png} alt={country.flags.flags?.alt} />
+			<p className="h1Style">Country Name: {country.name?.common}</p>
+			<p>Capital City: {country.capital?.capital}</p>
+			<p>Population: {country.population?.population}</p>
+
+			<p>
+				Currency: {currencyName} / Symbol: {currencySymbol}
+			</p>
+		</div>
+	);
 };
 
 export default Country;
